9dbfc6128aacea16b4bdca9d26af7062cc291452
Abbyrathore <thakurjay97@gmail.com>
2021-06-14 21:44:11 +0530

    Admin panel changes

---
+++ react-admin/src/pages/userList/UserList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./userList.css";
+
+export default function UserList() {
+  return (<div className="userList">
+    <div className="test">ffff</div>
+  </div>);
+}
